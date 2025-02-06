b810b0908cbd5496b25df527c2d903fda0a351f9
JeffColby <trungthanh25012002@gmail.com>
2025-02-06 17:22:05 +0700

    code màn hình splash và đăng nhập

---
+++ NewFashion/screens/SplashScreen2.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Image, View } from 'react-native'
+import React from 'react'
+
+const SplashScreen2 = () => {
+  return (
+      <View style={st.container}>
+          <Image
+              source={require('../src/image/img_banner1.png')}
+              style={st.image} />
+      </View>
+  )
+}
+
+export default SplashScreen2
+
+const st = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+})
