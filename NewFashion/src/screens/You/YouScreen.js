52cc05f10d61977a529e092fcf7613244e547ee2
ThienHoang <thien19dev@gmail.com>
2025-02-06 16:34:42 +0700

    Code thêm BottomTabNavigator

---
+++ NewFashion/src/screens/You/YouScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const YouScreen = () => {
+  return (
+    <View>
+      <Text>YouScreen</Text>
+    </View>
+  )
+}
+
+export default YouScreen
+
+const styles = StyleSheet.create({})
