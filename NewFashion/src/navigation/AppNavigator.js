5b37445b5fc1ce8d57faaf956c81c6bbedc84ac4
NhanHoo23 <hohuunhan2001@gmail.com>
2025-02-10 19:32:46 +0700

    code google sign in

--- NewFashion/src/navigation/AppNavigator.js
+++ NewFashion/src/navigation/AppNavigator.js
@@ -19,10 +19,10 @@ const AppNavigator = () => {
         {/* <Stack.Screen name="Test" component={TestScreen} /> */}
         {/* <Stack.Screen name="Splash1" component={SplashScreen1} /> */}
         {/* <Stack.Screen name="Splash2" component={SplashScreen2} /> */}
-        {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
+        <Stack.Screen name="Login" component={LoginScreen} />
         {/* <Stack.Screen name="RegisterWithEmail" component={RegisterWithEmailScreen} /> */}
         {/* <Stack.Screen name="LoginWithEmail" component={LoginWithEmail} /> */}
-        <Stack.Screen name="Main" component={MainScreen} />
+        {/* <Stack.Screen name="Main" component={MainScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   )
--- NewFashion/src/screens/LoginScreen.js
+++ NewFashion/src/screens/LoginScreen.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import BenefitsInfoBox from '../components/BenefitsInfoBox';
 import OutlinedButton from '../components/OutlinedButton';
 import ScreenSize from '../contants/ScreenSize';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
 const LoginScreen = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: "820669094821-sn7aobosoluohktj0gglaa80t1uk6rvi.apps.googleusercontent.com",
+    });
+  }, []);
+
+  const onGoogleButtonPress = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      
+      const { idToken } = await GoogleSignin.signIn();
+
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      await auth().signInWithCredential(googleCredential);
+
+      console.log('Đăng nhập thành công!');
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('Đăng nhập bị hủy');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('Đang đăng nhập');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('Google Play Services không có sẵn');
+      } else {
+        console.error(error);
+      }
+    }
+  }
+
   return (
     <View style={st.container}>
 
@@ -26,8 +58,8 @@ const LoginScreen = () => {
         <BenefitsInfoBox icon={require('../assets/ic_freeReturns.png')} title="Free returns" subtitle="Up to 90 days" />
         <BenefitsInfoBox icon={require('../assets/ic_freeShipping.png')} title="Free shipping" subtitle="On all orders" />
       </View>
-      
-      <OutlinedButton icon={require('../assets/bt_google.png')} title="Continue with Google" customStyle={{ width: ScreenSize.width - 40, marginTop: 40 }} />
+
+      <OutlinedButton icon={require('../assets/bt_google.png')} title="Continue with Google" customStyle={{ width: ScreenSize.width - 40, marginTop: 40 }} onPress={onGoogleButtonPress}/>
       <OutlinedButton icon={require('../assets/bt_facebook.png')} title="Continue with Facebook" customStyle={{ width: ScreenSize.width - 40, marginTop: 10 }} />
       <OutlinedButton icon={require('../assets/bt_email.png')} title="Continue with Email" customStyle={{ width: ScreenSize.width - 40, marginTop: 10 }} />
       <OutlinedButton icon={require('../assets/bt_phone.png')} title="Continue with phone number" customStyle={{ width: ScreenSize.width - 40, marginTop: 10 }} />
